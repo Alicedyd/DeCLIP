6638d76a4f959cbd190cc57dd917e51da120da07
Alicedyd <xjw20020626@126.com>
2024-12-15 20:46:55 +0800

    update .gitignore

--- create_masks.py
+++ create_masks.py
@@ -4,21 +4,21 @@ from tqdm import tqdm
 import os
 import numpy as np
 
-# subset_name = "stable-diffusion-2-inpainting"
+subset_name = "stable-diffusion-2-inpainting"
 
-# train_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/train2017".format(subset_name)
-# val_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/val2017".format(subset_name)
+train_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/train2017".format(subset_name)
+val_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/val2017".format(subset_name)
 
-# mask_train_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/masks/train2017".format(subset_name)
-# mask_val_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/masks/val2017".format(subset_name)
+mask_train_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/masks/train2017".format(subset_name)
+mask_val_path = "/root/autodl-tmp/AIGC_data/DRCT-2M/{}/masks/val2017".format(subset_name)
 
-train_path = "/root/autodl-tmp/AIGC_data/MSCOCO/train2017"
-val_path = "/root/autodl-tmp/AIGC_data/MSCOCO/val2017"
+# train_path = "/root/autodl-tmp/AIGC_data/MSCOCO/train2017"
+# val_path = "/root/autodl-tmp/AIGC_data/MSCOCO/val2017"
 
-mask_train_path = "/root/autodl-tmp/AIGC_data/MSCOCO/masks/train2017"
-mask_val_path = "/root/autodl-tmp/AIGC_data/MSCOCO/masks/val2017"
+# mask_train_path = "/root/autodl-tmp/AIGC_data/MSCOCO/masks/train2017"
+# mask_val_path = "/root/autodl-tmp/AIGC_data/MSCOCO/masks/val2017"
 
-real = True
+real = False
 if real:
     label = 0
 else:
@@ -48,5 +48,5 @@ def create_masks(img_folder, mask_folder):
             
             pbar.update(1)
             
-# create_masks(train_path, mask_train_path)
-create_masks(val_path, mask_val_path)
+create_masks(train_path, mask_train_path)
+# create_masks(val_path, mask_val_path)
