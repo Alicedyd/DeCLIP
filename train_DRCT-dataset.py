a9f8a1762cf846dde5880eb8ee435aac51186c66
Alicedyd <xjw20020626@126.com>
2024-12-27 12:22:02 +0800

    修改为DRCT数据集

--- train_DRCT-dataset.py
+++ train_DRCT-dataset.py
@@ -16,10 +16,7 @@ import torch.multiprocessing
 from data_DRCT.transform import create_train_transforms, create_val_transforms
 
 from tqdm import tqdm
-<<<<<<< HEAD
-=======
 from utils.visualize import *
->>>>>>> 237d3af5 (修改为DRCT数据集)
 
 def get_val_opt(opt):
     val_opt = deepcopy(opt)
@@ -30,11 +27,7 @@ def get_val_opt(opt):
 if __name__ == '__main__':
     torch.multiprocessing.set_sharing_strategy('file_system')
 
-<<<<<<< HEAD
-    opt = TrainOptions().parse()
-=======
     opt = TrainOptions().parse(print_options=False)
->>>>>>> 237d3af5 (修改为DRCT数据集)
 
     val_opt = get_val_opt(opt)
     is_one_hot = False
@@ -64,9 +57,6 @@ if __name__ == '__main__':
     start_time = time.time()
     best_iou = 0
     
-<<<<<<< HEAD
-    for epoch in range(opt.niter):
-=======
     visualize_mask=True ######
     if visualize_mask:
         # preparation for visualizing masks
@@ -76,16 +66,12 @@ if __name__ == '__main__':
         
     for epoch in range(opt.niter):
         torch.cuda.empty_cache()
->>>>>>> 237d3af5 (修改为DRCT数据集)
         print(f"Epoch {epoch}")
         epoch_loss = 0
         with tqdm(total=len(train_loader), desc=f"Epoch [{epoch}/{opt.niter}]", unit="batch", ncols=150) as pbar:
             for i, data in enumerate(train_loader):
-<<<<<<< HEAD
-=======
                 image, label, gd_masks, img_paths, cutmix_img_be_aug = data
                 
->>>>>>> 237d3af5 (修改为DRCT数据集)
                 model.total_steps += 1
 
                 model.set_input(data)
@@ -100,8 +86,6 @@ if __name__ == '__main__':
                     train_writer.add_scalar('loss', model.loss, model.total_steps)
                     
                 pbar.update(1)
-<<<<<<< HEAD
-=======
                 
                 ####### 可视化
                 in_tens = image.cuda()
@@ -119,13 +103,10 @@ if __name__ == '__main__':
                         
                 if visualize_mask and (model.total_steps % 100 == 0):
                     # visualize the masks
-                    for i, (mask, gd_mask) in enumerate(zip(pred_masks, gd_masks)):
+                    for i, (mask, gd_mask, l) in enumerate(zip(pred_masks, gd_masks, label)):
                         img_name = os.path.basename(img_paths[i])
                         img_prefix = img_name.split(".")[0]
-                        if 'MSCOCO' in img_paths[i]:
-                            file_name = f"{img_prefix}_0"
-                        else:
-                            file_name = f"{img_prefix}_1"
+                        file_name = f"{img_prefix}_{l}"
     
                         binary_mask = (mask > 0.5).float().cpu()
         
@@ -149,7 +130,6 @@ if __name__ == '__main__':
                             file_name=file_name,
                         )
 
->>>>>>> 237d3af5 (修改为DRCT数据集)
 
         epoch_loss /= len(train_loader)
         if opt.fully_supervised:
@@ -171,41 +151,27 @@ if __name__ == '__main__':
         elif opt.mask_plus_label:
             # xjw
             mean_iou = sum(model.ious)/len(model.ious)
-<<<<<<< HEAD
-=======
             train_writer.add_scalar('iou', mean_iou, model.total_steps)
->>>>>>> 237d3af5 (修改为DRCT数据集)
             model.ious = []
             print(f"Epoch mean train IOU: {round(mean_iou, 2)}")
             
             mean_F1_best = sum(model.F1_best)/len(model.F1_best)
-<<<<<<< HEAD
-            model.F1_best = []
-            print(f"Epoch mean train F1_best: {round(mean_F1_best, 4)}")
-            mean_F1_fixed = sum(model.F1_fixed)/len(model.F1_fixed)
-=======
             train_writer.add_scalar('F1_best', mean_F1_best, model.total_steps)
             model.F1_best = []
             print(f"Epoch mean train F1_best: {round(mean_F1_best, 4)}")
             
             mean_F1_fixed = sum(model.F1_fixed)/len(model.F1_fixed)
             train_writer.add_scalar('F1_fixed', mean_F1_fixed, model.total_steps)
->>>>>>> 237d3af5 (修改为DRCT数据集)
             model.F1_fixed = []
             print(f"Epoch mean train F1_fixed: {round(mean_F1_fixed, 4)}")
             
             model.format_output()
             mean_acc = compute_accuracy_detection(model.logits, model.labels)
-<<<<<<< HEAD
-            print(f"Epoch mean train ACC: {round(mean_acc, 2)}")
-            mean_ap = compute_average_precision_detection(model.logits, model.labels)
-=======
             train_writer.add_scalar('acc', mean_acc, model.total_steps)
             print(f"Epoch mean train ACC: {round(mean_acc, 2)}")
             
             mean_ap = compute_average_precision_detection(model.logits, model.labels)
             train_writer.add_scalar('ap', mean_ap, model.total_steps)
->>>>>>> 237d3af5 (修改为DRCT数据集)
             print(f"Epoch mean train AP: {round(mean_ap, 4)}")
         
             model.logits = []
@@ -284,15 +250,9 @@ if __name__ == '__main__':
             if ap > best_iou:
                 print('saving best model at the end of epoch %d' % (epoch))
                 print(ap, best_iou)
-<<<<<<< HEAD
-                model.save_networks( 'model_epoch_best.pth' )
-                best_iou = ap
-
-=======
                 model.save_networks(f'model_best_epoch_{epoch}_acc_{acc}.pth' )
                 best_iou = ap
             model.save_networks(f'model_last_epoch_{epoch}_acc_{acc}.pth' )
->>>>>>> 237d3af5 (修改为DRCT数据集)
             early_stopping(acc, model)
         
         if early_stopping.early_stop:
